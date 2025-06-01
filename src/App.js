c2b5946321c9d2c102bded5ef89864875b9f1617
AbdullahAdil145 <abdullahadil143@gmail.com>
2025-06-01 03:07:10 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hi, I'm Muhammad Abdullah Adil</h1>
+      <p>Welcome to my Website!</p>
     </div>
   );
 }
-
 export default App;
