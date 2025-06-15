08f48657a1a98e942f694ce04c727d22c0efe1a0
Abdullah Adil <abdullahadil143@gmail.com>
2025-06-15 02:53:25 -0400

    left align on exp pts

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
     <h3>TBD</h3>
     <h2>Junior Software Developer, Founding Team</h2>
     <section style={{ maxWidth: '800px', margin: '0 auto', textalign:'left'}}>
-      <ul style={{ listStyleType: 'disc', paddingLeft: '1.5rem' }}>
+      <ul style={{ listStyleType: 'disc', paddingLeft: '1.5rem', textalign:'left' }}>
         <li>Developed a full stack prototype for a job application automation service.</li>
         <li>Collaborated on making Chrome extensions to help autofill job applications across hundreds of websites.</li>
         <li>Used web scraping to find job postings across different job boards.</li>
